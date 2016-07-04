c630084419fc89badf52531c2cd76634dbe92741
Jan <jan@komoot.de>
2016-07-04 16:40:12 +0200

    added setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='yamlcf',      # This is the name of your PyPI-package.
+    version='0.2',      # Update the version number for new releases
+    scripts=['yamlcf.py'],  # The name of your script, and also the command you'll be using for calling it
+    url='https://github.org/komoot/yamlcf',
+    author='Jan Heuer',
+    author_email='jan@komoot.de'
+)
