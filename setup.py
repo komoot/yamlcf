4cdab7595bb8b1226cbe405adb9e1039b6a8d37a
Christoph Lingg <christoph@lingg.eu>
2017-01-24 14:04:38 +0100

    correct url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='yamlcf',      # This is the name of your PyPI-package.
     version='0.2',      # Update the version number for new releases
     scripts=['yamlcf.py'],  # The name of your script, and also the command you'll be using for calling it
-    url='https://github.org/komoot/yamlcf',
+    url='https://github.com/komoot/yamlcf',
     author='Jan Heuer',
     author_email='jan@komoot.de',
     install_requires=[
