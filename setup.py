2af170cd97086fc97f025164dc33a7eef0b050aa
Christoph Lingg <christoph@lingg.eu>
2017-01-24 12:42:05 +0100

    add dependencies

--- setup.py
+++ setup.py
@@ -6,5 +6,9 @@ setup(
     scripts=['yamlcf.py'],  # The name of your script, and also the command you'll be using for calling it
     url='https://github.org/komoot/yamlcf',
     author='Jan Heuer',
-    author_email='jan@komoot.de'
+    author_email='jan@komoot.de',
+    install_requires=[
+        'pyyaml',
+        'botocore'
+    ]
 )
